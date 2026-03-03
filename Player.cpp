7f06609ddfda7af550ccbaedfdb46912ef1ae19f
SuperGuy123456 <111657887+SuperGuy123456@users.noreply.github.com>
2026-03-02 20:45:28 -0800

    updated the spritesheet and added better animation systems

--- Player.cpp
+++ Player.cpp
@@ -12,16 +12,27 @@ Player::Player(Vector2 pos, DrawLayer& _entitylayer) : entitylayer(_entitylayer)
     allframes = SpriteSplitter::SplitByHorizontal("../Art/Player/ninjaframes.png", 15, 10);
     TraceLog(LOG_INFO, "Player: Loaded %d frames", allframes.size());
 
-    allsequences.push_back(&idle);
-    allsequences.push_back(&walk);
-    allsequences.push_back( &run);
-    allsequences.push_back(&idle_ready);
-    allsequences.push_back(&attack1);
-    allsequences.push_back(&attack2);
+    allsequences.push_back(&idle);          // 0
+    allsequences.push_back(&readyidle);     // 1
+    allsequences.push_back(&idle_ready);    // 2  ← moved here
+    allsequences.push_back(&walk);          // 3
+    allsequences.push_back(&readywalk);     // 4
+    allsequences.push_back(&run);           // 5
+    allsequences.push_back(&readyrun);      // 6
+    allsequences.push_back(&attack1);       // 7
+    allsequences.push_back(&attack2);       // 8
+    allsequences.push_back(&upjump);        // 9
+    allsequences.push_back(&falling);       // 10
+    allsequences.push_back(&readyupjump);   // 11
+    allsequences.push_back(&readyfalling);  // 12
+    allsequences.push_back(&land_idle);     // 13
+    allsequences.push_back(&land_ready);    // 14
+
+
 
     int fullindex = 0;
 
-    for (int i = 0; i < 6; ++i) {
+    for (int i = 0; i < NUMBEROFSTATES; ++i) {
         for (int j = 0; j < sizes[i]; ++j) {
             allsequences[i]->push_back(&allframes[fullindex]);
             fullindex++;
@@ -42,6 +53,7 @@ void Player::Draw() {
 
 void Player::Update() {
     Animate();
+    canim = LAND_READY;
 }
 
 void Player::OnEvent(string &command) {
@@ -54,25 +66,25 @@ void Player::OnSpecialEvent(string &command, vector<string> params) {
 
 void Player::Animate()
 {
+    // if the animation changed externally (input), reset frame index
+    static int lastAnim = -1;
+    if (canim != lastAnim) {
+        cindex = 0;
+        lastAnim = canim;
+    }
+
+    // frame timing
     if (GetTime() - lasttime >= 0.2f)
     {
         lasttime = GetTime();
-
-        // advance frame
         cindex++;
 
-        // wrap around based on animation size
+        // loop animation
         if (cindex >= sizes[canim])
         {
             cindex = 0;
-            //temporarily update the canim
-            if (canim < 5) {
-                canim++;
-            }
-            else {
-                canim = 0;
-            }
         }
     }
 }
+
 
--- Player.h
+++ Player.h
@@ -13,6 +13,25 @@
 
 using namespace std;
 
+enum AnimState {
+    IDLE = 0,
+    READYIDLE = 1,
+    IDLE_READY = 2,     // ← moved here
+    WALK = 3,
+    READYWALK = 4,
+    RUN = 5,
+    READYRUN = 6,
+    ATTACK1 = 7,
+    ATTACK2 = 8,
+    UPJUMP = 9,
+    FALLING = 10,
+    READYUPJUMP = 11,
+    READYFALLING = 12,
+    LAND_IDLE = 13,
+    LAND_READY = 14
+};
+
+
 class Player : public Load, public Listener
 {
 public:
@@ -37,23 +56,61 @@ private:
     double lasttime = GetTime();
 
     //magic nums for animations
+    const int NUMBEROFSTATES = 15;
+
     const int IDLESIZE = 4;
+    const int READYIDLESIZE = 2;
     const int WALKSIZE = 6;
+    const int READYWALKSIZE = 6;
     const int RUNSIZE = 6;
+    const int READYRUNSIZE = 6;
     const int IDLE_READYSIZE = 6;
     const int ATTACK1SIZE = 3;
     const int ATTACK2SIZE = 4;
+    const int UPJUMPSIZE = 1;
+    const int FALLINGSIZE = 1;
+    const int READYUPJUMPSIZE = 1;
+    const int READYFALLINGSIZE = 1;
+    const int LAND_IDLESIZE = 2;
+    const int LAND_READYSIZE = 2;
 
-    const int sizes[6] = {IDLESIZE, WALKSIZE, RUNSIZE, IDLE_READYSIZE, ATTACK1SIZE, ATTACK2SIZE};
+    const int sizes[15] = {
+        IDLESIZE,
+        READYIDLESIZE,
+        IDLE_READYSIZE,   // moved here
+        WALKSIZE,
+        READYWALKSIZE,
+        RUNSIZE,
+        READYRUNSIZE,
+        ATTACK1SIZE,
+        ATTACK2SIZE,
+        UPJUMPSIZE,
+        FALLINGSIZE,
+        READYUPJUMPSIZE,
+        READYFALLINGSIZE,
+        LAND_IDLESIZE,
+        LAND_READYSIZE
+    };
+
 
     vector<Texture2D> allframes;
 
-    vector<Texture2D*> idle;
-    vector<Texture2D*> walk;
-    vector<Texture2D*> run;
-    vector<Texture2D*> idle_ready;
-    vector<Texture2D*> attack1;
-    vector<Texture2D*> attack2;
+    vector<Texture2D*> idle;             // 0
+    vector<Texture2D*> readyidle;        // 1
+    vector<Texture2D*> walk;             // 2
+    vector<Texture2D*> readywalk;        // 3
+    vector<Texture2D*> run;              // 4
+    vector<Texture2D*> readyrun;         // 5
+    vector<Texture2D*> idle_ready;       // 6
+    vector<Texture2D*> attack1;          // 7
+    vector<Texture2D*> attack2;          // 8
+    vector<Texture2D*> upjump;           // 9
+    vector<Texture2D*> falling;          // 10
+    vector<Texture2D*> readyupjump;      // 11
+    vector<Texture2D*> readyfalling;     // 12
+    vector<Texture2D*> land_idle;        // 13
+    vector<Texture2D*> land_ready;       // 14
+
 
     vector<vector<Texture2D*>*> allsequences;
 };
